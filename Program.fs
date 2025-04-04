509d252db4935693b6273dfe34d8cd695dd3a6b0
Joost Kaptein <joost@compositional-it.com>
2025-04-04 05:58:11 +0100

    working demo

--- Program.fs
+++ Program.fs
@@ -1,2 +1,73 @@
-﻿// For more information see https://aka.ms/fsharp-console-apps
-printfn "Hello from F#"
+﻿open Feliz
+
+
+open Browser.Dom
+
+module Elmish =
+    open Feliz.UseElmish
+    open Elmish
+
+    type Model =
+        { counter: int
+          mutable disposed: bool }
+
+        interface System.IDisposable with
+            member this.Dispose() = this.disposed <- true
+
+    type Msg =
+        | Poll
+        | IncrementCounters
+
+    let init _ =
+        { counter = 0; disposed = false }, Cmd.ofMsg Poll
+
+    let update updateCounter msg model =
+        match msg with
+        | IncrementCounters ->
+            updateCounter ((+) 1)
+
+            { model with
+                counter = model.counter + 1 },
+            Cmd.none
+
+        | Poll ->
+            printfn "polling"
+
+            let nextPoll =
+                match model.disposed with
+                | true -> Cmd.none
+                | false ->
+                    Cmd.OfAsync.perform
+                        (fun _ ->
+                            async {
+                                do! Async.Sleep 1000
+
+                            })
+                        ()
+                        (fun _ -> Poll)
+
+            model, Cmd.batch [ nextPoll; Cmd.ofMsg IncrementCounters ]
+
+    [<ReactComponent>]
+    let View updateCounter =
+        let model, _ = React.useElmish (init, update updateCounter, [])
+
+        Html.p $"inner counter: {model.counter}"
+
+[<ReactComponent>]
+let Body () =
+    let showingElmish, setShowingElmish = React.useStateWithUpdater false
+    let counter, setCounter = React.useStateWithUpdater 0
+
+    React.fragment
+        [ Html.p $"counter {counter}"
+          Html.button [ prop.text "toggle elmish"; prop.onClick (fun v -> setShowingElmish (not)) ]
+          if showingElmish then
+              Elmish.View setCounter
+          else
+              Html.p "not showing elmish loop" ]
+
+
+
+let root = ReactDOM.createRoot (document.getElementById "app")
+root.render (Body())
--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,8 @@ import fable from "vite-plugin-fable";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [fable()],
+    plugins: [fable()],
+    server: {
+        port: 8080
+    }
 });
